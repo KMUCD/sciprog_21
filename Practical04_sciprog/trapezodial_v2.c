f5efd383286e0d9da15ee766318d29b87e2ee9c6
Kevin <kevin.mongey@ucdconnect.ie>
2021-10-29 12:27:51 +0100

    Committing Practical04 - Trapezodial v2 and v3 c files and compiled files. v3 has the trapezodial rule written as its own function

---
+++ Practical04_sciprog/trapezodial_v2.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<math.h>
+
+float degtorad(float degang);
+int main(){
+   int N=12, i;
+   float TanArr[N+1], deg, rad;
+   
+   //Calcualte the values of the function from i=0 to i=12
+   //We want to use arrays this time and ensure xi is in radians
+   for(i=0; i<=N; i++){
+	deg = i*5.0;
+	//Call the degrees to radians functions from below
+	rad=degtorad(deg);
+	TanArr[i]=tan(rad);
+	printf("TanArr[%d] = %f (f(x) at x = %d)\n", i, TanArr[i], i);
+	}
+
+//Next, recalling last week, we use the trapezoidal
+//rule to find the area of our function 
+
+   float area;
+   area =TanArr[0] + TanArr[N];
+   for(i=1;i<N;i++){
+	area = area + 2.0*TanArr[i];
+   	}
+
+//Next we multiply the area by (pi/3)/2N after the 
+//conversion to radians. We then print our approx. result
+   float mult_rad = degtorad((60.0-0.0)/(2.0*N));
+   area = mult_rad*area;
+   printf("\nTrapezodial result:%f\n",area);
+
+//Print actual result,ln(2) (natural log)
+   printf("Real result:%f\n",log(2.0));
+
+   return 0;
+
+}
+
+
+//Here we define the float to radians function
+//This converts degrees to radians - Does what it says on the tin!
+float degtorad(float degang){
+   return ((M_PI*degang)/180.0);
+}
